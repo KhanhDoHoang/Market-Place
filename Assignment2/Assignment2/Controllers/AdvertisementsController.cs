296eadcc0287d23dcad37313dd11a9ae3d2295a5
Hoang Do <katievincy226@gmail.com>
2022-03-31 23:30:24 -0400

    Add view model controller for advertisement and view models

--- Assignment2/Assignment2/Controllers/AdvertisementsController.cs
+++ Assignment2/Assignment2/Controllers/AdvertisementsController.cs
@@ -2,30 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Assignment2.Data;
-using Assignment2.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Assignment2.Data;
+using Assignment2.Models;
 
-namespace Assignment2.Controllers
+namespace Assignment2.Views
 {
-    public class ClientsController : Controller
+    public class AdvertisementsController : Controller
     {
         private readonly MarketDbContext _context;
 
-        public ClientsController(MarketDbContext context)
+        public AdvertisementsController(MarketDbContext context)
         {
             _context = context;
         }
 
-        // GET: Clients
+        // GET: Advertisements
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Clients.ToListAsync());
+            var marketDbContext = _context.Advertisements.Include(a => a.Brokerage);
+            return View(await marketDbContext.ToListAsync());
         }
 
-        // GET: Clients/Details/5
+        // GET: Advertisements/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -33,43 +34,42 @@ namespace Assignment2.Controllers
                 return NotFound();
             }
 
-            var client = await _context.Clients
+            var advertisement = await _context.Advertisements
+                .Include(a => a.Brokerage)
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (client == null)
+            if (advertisement == null)
             {
                 return NotFound();
             }
 
-            return View(client);
+            return View(advertisement);
         }
 
-        // GET: Clients/Create
+        // GET: Advertisements/Create
         public IActionResult Create()
         {
-            ViewData["ClientId"] = new SelectList(_context.Clients, "Id", "FirstName");
-
+            ViewData["BrokerageId"] = new SelectList(_context.Brokerages, "Id", "Id");
             return View();
         }
 
-        // POST: Clients/Create
+        // POST: Advertisements/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,LastName,FirstName,BirthDate")] Client client)
+        public async Task<IActionResult> Create([Bind("Id,FileName,Url,BrokerageId")] Advertisement advertisement)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(client);
+                _context.Add(advertisement);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["ClientId"] = new SelectList(_context.Clients, "Id", "FirstName");
-
-            return View(client);
+            ViewData["BrokerageId"] = new SelectList(_context.Brokerages, "Id", "Id", advertisement.BrokerageId);
+            return View(advertisement);
         }
 
-        // GET: Clients/Edit/5
+        // GET: Advertisements/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -77,24 +77,23 @@ namespace Assignment2.Controllers
                 return NotFound();
             }
 
-            var client = await _context.Clients.FindAsync(id);
-            if (client == null)
+            var advertisement = await _context.Advertisements.FindAsync(id);
+            if (advertisement == null)
             {
                 return NotFound();
             }
-            ViewData["ClientId"] = new SelectList(_context.Clients, "Id", "FirstName");
-
-            return View(client);
+            ViewData["BrokerageId"] = new SelectList(_context.Brokerages, "Id", "Id", advertisement.BrokerageId);
+            return View(advertisement);
         }
 
-        // POST: Clients/Edit/5
+        // POST: Advertisements/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,LastName,FirstName,BirthDate")] Client client)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FileName,Url,BrokerageId")] Advertisement advertisement)
         {
-            if (id != client.Id)
+            if (id != advertisement.Id)
             {
                 return NotFound();
             }
@@ -103,12 +102,12 @@ namespace Assignment2.Controllers
             {
                 try
                 {
-                    _context.Update(client);
+                    _context.Update(advertisement);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!ClientExists(client.Id))
+                    if (!AdvertisementExists(advertisement.Id))
                     {
                         return NotFound();
                     }
@@ -119,12 +118,11 @@ namespace Assignment2.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["ClientId"] = new SelectList(_context.Clients, "Id", "FirstName");
-
-            return View(client);
+            ViewData["BrokerageId"] = new SelectList(_context.Brokerages, "Id", "Id", advertisement.BrokerageId);
+            return View(advertisement);
         }
 
-        // GET: Clients/Delete/5
+        // GET: Advertisements/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -132,35 +130,31 @@ namespace Assignment2.Controllers
                 return NotFound();
             }
 
-            var client = await _context.Clients
+            var advertisement = await _context.Advertisements
+                .Include(a => a.Brokerage)
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (client == null)
+            if (advertisement == null)
             {
                 return NotFound();
             }
 
-            return View(client);
+            return View(advertisement);
         }
 
-        // POST: Clients/Delete/5
+        // POST: Advertisements/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var client = await _context.Clients.FindAsync(id);
-            _context.Clients.Remove(client);
+            var advertisement = await _context.Advertisements.FindAsync(id);
+            _context.Advertisements.Remove(advertisement);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
-
-        //todo: enable user to register/unregister clients to brokerages
-        //which is basically calling the subscription based on clientId and remove the id of that brokerages
-        //three action controls are listing(get many)/remove/add
-
-        private bool ClientExists(int id)
+        private bool AdvertisementExists(int id)
         {
-            return _context.Clients.Any(e => e.Id == id);
+            return _context.Advertisements.Any(e => e.Id == id);
         }
     }
 }
--- Assignment2/Assignment2/Models/ViewModels/BrokerageViewModel.cs
+++ Assignment2/Assignment2/Models/ViewModels/BrokerageViewModel.cs
@@ -6,4 +6,31 @@
         public IEnumerable<Brokerage> Brokerages { get; set; }
         public IEnumerable<Subscription> Subscriptions { get; set; }
     }
+
+    public class FileInputViewModel
+    {
+        public string BrokerageId { get; set; }
+        public string BrokerageTitle { get; set; }
+        public IFormFile File { get; set; }
+    }
+
+    public class AdsViewModel
+    {
+        public Brokerage Brokerage { get; set; }
+        public IEnumerable<Advertisement> Advertisements { get; set; }
+    }
+
+    public class ClientSubscriptionsViewModel
+    {
+        public Client Client { get; set; }
+        public IEnumerable<BrokerageSubscriptionsViewModel> Subscriptions { get; set; }
+    }
+
+    public class BrokerageSubscriptionsViewModel
+    {
+        public string BrokerageId { get; set; }
+        public string Title { get; set; }
+        public bool IsMember { get; set; }
+    }
+
 }
