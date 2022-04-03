4c6943888f5da3b37223cfc435631ae99f6d9acf
Hoang Do <katievincy226@gmail.com>
2022-04-03 14:30:43 -0400

    Client Controller in progress

--- Assignment2/Assignment2/Controllers/ClientsController.cs
+++ Assignment2/Assignment2/Controllers/ClientsController.cs
@@ -181,6 +181,133 @@ namespace Assignment2.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        // GET: Clients/Edit/5
+        public async Task<IActionResult> EditSubscriptions(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            //ViewData["ClientId"] = id;
+
+            var viewModel = new ClientSubscriptionsViewModel
+            {
+                Client = await _context.Clients.FindAsync(id),
+            };
+
+            if (viewModel.Client == null)
+            {
+                return NotFound();
+            }
+
+            IList<Brokerage> BrokerageList = await _context.Brokerages.ToListAsync(); //list of brokerages 
+           
+            //model view list of all brokerages (check IsMember)
+            IList<BrokerageSubscriptionsViewModel> SubscriptionList = new List<BrokerageSubscriptionsViewModel>();
+
+            //registered subscription
+            //IList<Subscription> RegisteredSubscriptionList = await _context.Subscriptions.Where(subscription => id.Equals(subscription.ClientId)).ToListAsync();
+            //registered subscriptions
+            IList<Subscription> RegisteredSubscriptions = viewModel.Client.Subscriptions;
+            RegisteredSubscriptions.ToList().ForEach(subscription => 
+                subscription.Brokerage = BrokerageList.ToList().Where((brokerage) => subscription.BrokerageId == brokerage.Id).Single()
+            );
+
+
+            BrokerageList.ToList().ForEach(brokerage =>
+            {
+                BrokerageSubscriptionsViewModel subscriptionModel = new BrokerageSubscriptionsViewModel();
+                Subscription subscription = brokerage.Subscriptions.Where(subscription => subscription.BrokerageId.Equals(brokerage.Id)).Single();
+                if (id.Equals(subscription.ClientId)) //clientId == subscriptionId
+                {
+
+                } else
+                {
+
+                }
+                //add to subscriptionList
+                SubscriptionList.Add(subscription);
+
+            });
+            //ViewData["ClientId"] = new SelectList(_context.Clients, "Id", "FirstName");
+
+            return View(viewModel);
+        }
+
+        // POST: Clients/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddSubscriptions(int clientId, string brokerageId, [Bind("Id,LastName,FirstName,BirthDate")] Client client)
+        {
+            if (id != client.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(client);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClientExists(client.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClientId"] = new SelectList(_context.Clients, "Id", "FirstName");
+
+            return View(client);
+        }
+
+        // POST: Clients/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RemoveSubscriptions(int clientId, string brokerageId, [Bind("Id,LastName,FirstName,BirthDate")] Client client)
+        {
+            if (id != client.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(client);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClientExists(client.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClientId"] = new SelectList(_context.Clients, "Id", "FirstName");
+
+            return View(client);
+        }
 
         //todo: enable user to register/unregister clients to brokerages
         //which is basically calling the subscription based on clientId and remove the id of that brokerages
